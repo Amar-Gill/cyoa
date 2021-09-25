3e68d020149297cfdebeb14d6096823ffa03ab62
Amar Gill <amargill@Amars-MacBook-Air.local>
2021-09-25 16:51:38 -0400

    can parse json file. render Intro chapter into webpage

--- main.go
+++ main.go
@@ -1,23 +1,48 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 )
 
+type Chapter struct {
+	Title   string
+	Story   []string
+	Options []Option
+}
+
+type Option struct {
+	Text    string
+	Chapter string
+}
 type PageData struct {
-	PageTitle string
-	SomeProp  string
+	Intro     Chapter
+	NewYork   Chapter
+	Debate    Chapter
+	SeanKelly Chapter
+	MarkBates Chapter
+	Home      Chapter
 }
 
 func main() {
+	jsonBytes, err := os.ReadFile("gopher.json")
+	if err != nil {
+		panic(err)
+	}
+
+	var data PageData
+
+	err = json.Unmarshal(jsonBytes, &data)
+	if err != nil {
+		panic(err)
+	}
+
 	tmpl := template.Must(template.ParseFiles("template.html"))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data := PageData{
-			PageTitle: "Cool Page Title",
-			SomeProp:  "Cool property",
-		}
 		tmpl.Execute(w, data)
 	})
 	fmt.Println("Listening on port :8080")
